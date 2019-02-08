651aa74ad9cf7cccadbc44da5bd6979537a49819
Julian Katz <juliankatz1991@gmail.com>
2019-02-08 16:29:28 -0600

    got serverless offline going with basic github request

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+// dependencies
 const serverless = require('serverless-http')
 const express = require('express')
+const axios = require('axios')
+
+// Routes
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('hello bla')
 })
+
+app.get('/followers/:githubID', async function (req, res) {
+  const githubID = req.params.githubID
+
+  const output = { [githubID]: {} }
+
+  const followers = await getFollowers(githubID)
+
+  followers.forEach(follower => {
+    output[githubID][follower] = {}
+  })
+
+  res.send(output)
+})
+
+const BASE_URL = 'https://api.github.com'
+async function getFollowers (userLogin) {
+  const url = BASE_URL + `/users/${userLogin}/followers?page=1&per_page=5`
+
+  const results = await axios(url)
+  return results.data.map(follower => {
+    return follower.login
+  })
+}
 
 module.exports.handler = serverless(app)
