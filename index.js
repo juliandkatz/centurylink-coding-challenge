e89bb3a99773e34894883b94e4823d2d41ce5177
Julian Katz <juliankatz1991@gmail.com>
2019-02-08 18:09:24 -0600

    first query is working and moved into its own file

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
 // dependencies
 const serverless = require('serverless-http')
 const express = require('express')
-const axios = require('axios')
+
+// local dependencies
+const getFollowerTree = require('./follower-tree.js')
 
 // Routes
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('hello bla')
+  res.send('Hello World')
 })
 
 app.get('/followers/:githubID', async function (req, res) {
-  const githubID = req.params.githubID
+  const baseUserID = req.params.githubID
 
-  const output = { [githubID]: {} }
-
-  const followers = await getFollowers(githubID)
-
-  followers.forEach(follower => {
-    output[githubID][follower] = {}
-  })
+  const output = await getFollowerTree(baseUserID)
 
   res.send(output)
 })
-
-const BASE_URL = 'https://api.github.com'
-async function getFollowers (userLogin) {
-  const url = BASE_URL + `/users/${userLogin}/followers?page=1&per_page=5`
-
-  const results = await axios(url)
-  return results.data.map(follower => {
-    return follower.login
-  })
-}
 
 module.exports.handler = serverless(app)
